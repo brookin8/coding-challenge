fa9c060fe66241762aa4fe801168795a29fe522a
Erin Wilcox <brookin8@gmail.com>
2019-02-24 15:14:07 -0500

    Update project to use Postgres

--- referralmanager/referralmanager/settings.py
+++ referralmanager/referralmanager/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'referralmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'referral_manager',
+        'USER': 'referraladmin',
+        'PASSWORD': 'ambassador',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
