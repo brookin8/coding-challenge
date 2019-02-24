31aac51afd2c5b7c64e98a162e16fdb2ba237c0d
Erin Wilcox <brookin8@gmail.com>
2019-02-24 17:02:36 -0500

    Configuring project for Heroku

--- referralmanager/referralmanager/settings.py
+++ referralmanager/referralmanager/settings.py
@@ -15,15 +15,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'an)6g34x_t^^@3dx*mey#pjluvh=c#g2fpw43zi7q=!is!#@%v'
+# SECRET_KEY = 'an)6g34x_t^^@3dx*mey#pjluvh=c#g2fpw43zi7q=!is!#@%v'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'an)6g34x_t^^@3dx*mey#pjluvh=c#g2fpw43zi7q=!is!#@%v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,12 +45,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ]
 
 REST_FRAMEWORK = {
@@ -78,17 +82,30 @@ WSGI_APPLICATION = 'referralmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+# DATABASES = {
+#    'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'referral_manager',
+#        'USER': 'referraladmin',
+#        'PASSWORD': 'ambassador',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432'
+#    }
+# }
+
+DATABASES = {
+    'default': os.environ.get(('DATABASE_URL'), 
+        {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'referral_manager',
         'USER': 'referraladmin',
         'PASSWORD': 'ambassador',
         'HOST': '127.0.0.1',
         'PORT': '5432'
-    }
+        }
+    )
 }
 
 
@@ -127,5 +144,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
