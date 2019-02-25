efb4384ccf3717c323f8a478e303e66bd4e3e00d
Erin Wilcox <brookin8@gmail.com>
2019-02-24 20:42:52 -0500

    Debug Heroku

--- referralmanager/referralmanager/wsgi.py
+++ referralmanager/referralmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'referralmanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'referralmanager.referralmanager.settings')
 
 application = get_wsgi_application()
